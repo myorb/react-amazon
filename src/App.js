bf29fd69d25b722c537613055b9d73982dad5ee7
alex <itdep@gmail.com>
2020-08-25 22:10:29 +0300

    add header styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from './Header'
+
 
 function App() {
   return (
-    <div className="App">
-        <h1>
-          This is start
-        </h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checout">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/">
+            <Header></Header>
+            <h1>Home</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'
+function Header() {
+    return (
+        <nav className="header"> 
+            <Link to="/">
+                <img className="header__logo" src="/logo192.png" />
+            </Link>
+            <div className="header__search">
+                <imput type="text" className="header__serachImput" />
+                <SearchIcon className="header__serachIcon" />
+            </div>
+            <div className="header__nav">
+                <Link to="/login" className="header__link">
+                    <div className="header__option">
+                        <span className="header__optionFirst">Hello alex</span>
+                        <span className="header__optionSecond">Sign in</span>
+                    </div>
+                </Link>
+                <Link to="/" className="header__link">
+                    <div className="header__option">
+                        <span className="header__optionFirst">Returns</span>
+                        <span className="header__optionSecond">Orders</span>
+                    </div>
+                </Link>
+                <Link to="/" className="header__link">
+                    <div className="header__option">
+                        <span className="header__optionFirst">Your</span>
+                        <span className="header__optionSecond">Prime</span>
+                    </div>
+                </Link>
+                <Link to="/checkout" className="header__link">
+                    <div className="header__optionBasket">
+                        <ShoppingBasketIcon className="header__optionBasket" />
+                        <span className="header__optionBasketCount">0</span>
+                    </div>
+                </Link>
+            </div>
+        </nav>
+    )
+}
+
+export default Header
